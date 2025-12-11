b5e316f7c7ada784f9bc814e0f269132ed85e94f
Ashish Rao <ashishrachakonda@gmail.com>
2025-12-11 15:38:14 -0500

    ex-1

--- 2.python-basics/1-ExerciseOperatorPrecedence.py
+++ 2.python-basics/1-ExerciseOperatorPrecedence.py
@@ -2,12 +2,16 @@
 # Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
 
 print((5 + 4) * 10 / 2)
+print("answer: 45")
 
 print(((5 + 4) * 10) / 2)
+print("answer: 45")
 
 print((5 + 4) * (10 / 2))
+print("answer: 45")
 
 print(5 + (4 * 10) / 2)
+print("answer: 25")
 
 print(5 + 4 * 10 // 2)
-
+print("answer: 25")
